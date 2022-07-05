0815de4e0bf7a1ab9d6da7a839bf1f2f946c5d73
michael glaviano <michael.glaviano@gmail.com>
2022-07-05 01:41:37 +0000

    migrating to local dev

--- src/lib/Horse.svelte
+++ src/lib/Horse.svelte
@@ -1,14 +1,45 @@
+<script>
+	import Ribbon from '$lib/Ribbon.svelte';
+	import { triumph } from '../stores.js';
+	let currTriumph;
+	triumph.subscribe((v) => {
+		currTriumph = v;
+	});
+</script>
 
+<div class="container">
+	<h1>curriculum vitae</h1>
+	<div class="hide-container">
+		<div class="hide">{currTriumph}</div>
+	</div>
+	<div class="img-container">
+		<img
+			src="cv.jpg"
+			alt="A show pony with its face completely obscured by dozens of county fair ribbons"
+		/>
+	<Ribbon />
+	</div>
+</div>
 
-<h1>curriculum vitae</h1>
-	<img src="cv.jpg" alt="A show pony with its face completely obscured by dozens of county fair ribbons">
-
-
-	<style>
+<style>
 	img {
-		margin-top: 1vw;
+		margin-top: 2vw;
 		max-height: 75vh;
 		max-width: 90vw;
 		border-radius: 50%;
 	}
-	</style>
+	.hide-container {
+		height: 1.5vh;
+	}
+
+	.hide {
+		display: none;
+	}
+
+	.hide-container:hover .hide {
+		display: inline;
+	}
+	.img-container {
+		position: relative;
+	}
+</style>
---
+++ src/lib/Ribbon.svelte
@@ -0,0 +1,20 @@
+<script>
+	import { triumph } from '../stores.js'
+	export let l;
+export let r;
+	
+</script>
+
+<div></div>
+
+	<style>
+	div {
+		z-index: 100;
+		position: absolute;
+		left: 183px;
+		top: 173px;
+		width: 25px;
+		height: 25px;
+		outline: 5px red dotted;
+	}
+	</style>
