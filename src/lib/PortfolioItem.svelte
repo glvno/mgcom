defa20e34fd7a1efda28fa1c0be25f93c3104c85
michael glaviano <michael.glaviano@gmail.com>
2025-11-20 12:09:03 -0600

    Portfolio updates

--- src/lib/PortfolioItem.svelte
+++ src/lib/PortfolioItem.svelte
@@ -3,7 +3,10 @@
 </script>
 
 <li class="flex w-full flex-col gap-3 pb-8">
-	<div class="fira font-bold">{item.title}</div>
+	<div class="flex flex-col gap-2">
+		<div class="fira font-bold">{item.title}</div>
+		<div class="fira text-xs">{item.tech}</div>
+	</div>
 	<img src={item.image} />
 
 	<div class="text-sm">{item.description}</div>
--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -7,39 +7,45 @@
 			title: 'cineplex',
 			uri: 'https://github.com/glvno/cineplex',
 			image: '/cineplex.gif',
-			description: "The world's least worst video multiplexer, written in Rust."
+			description: "The world's least worst video multiplexer, written in Rust.",
+			tech: 'Rust, iced'
 		},
 		{
 			title: 'unleaded.management',
 			uri: 'https://unleaded.management',
 			image: '/unleaded.jpg',
-			description: 'Replace every non-engineer at your company, yourself included, with LLMs.'
+			description: 'Replace every non-engineer at your company, yourself included, with LLMs.',
+			tech: 'SvelteKit, Postgres, RAG w/ pgvector'
 		},
 		{
 			title: 'raining.social',
 			uri: '/',
 			image: '/raining.jpg',
-			description: "Weather-gated social media. If it ain't rainin, you ain't scrollin."
+			description: "Weather-gated social media. If it ain't rainin, you ain't scrollin.",
+			tech: 'Elixir, Phoenix, Svelte, Postgres'
 		},
 
 		{
 			title: 'programming rust, 3rd edition (technical reviewer)',
 			uri: '/',
 			image: '/3e.jpg',
-			description: "Not sure why they let anywhere near this, but I'm glad they did."
+			description: "Not sure why they let anywhere near this, but I'm glad they did.",
+			tech: 'pdf sticky notes lol'
 		},
 		{
 			title: 'randy: game-theory-optimal poker made less impossible',
 			uri: '/',
 			image: '/randy.jpg',
 			description:
-				"He'll give you anything you want, so long as you want a random integer from 0 to 99."
+				"He'll give you anything you want, so long as you want a random integer from 0 to 99.",
+			tech: 'Rust, Tauri'
 		},
 		{
 			title: 'rocketballs',
 			uri: '/',
 			image: '/ballgame.gif',
-			description: 'More slide = more higher'
+			description: 'More slide = more higher',
+			tech: 'C#, Unity, prototyped in Rust w/ Bevy'
 		}
 	];
 </script>
